98a2e08007b945adb3d6e0af90a1e8449a2cbd57
deristnoor <deris.t.noor@gmail.com>
2020-07-24 11:53:43 +0700

    excellent

---
+++ var_keyword/var_keyword.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// DECLARE the variable "y"
+// ASSIGN the value 43
+// declare and assign = initilliazation
+var y = 43
+
+// DECLARE there is a VARIABLE with the IDENTIFIER "z"
+// and that the VARIABLE with the IDENTIFIER "z" is of TYPE int
+//false for booleans. 0 for integers, 0.0 for floats, "" for strings,
+// and nil for pointers, functions, interfaces, slices, channels, and maps.
+var z int
+
+func main() {
+	// short declaration operator
+	// DECLARE a variable and ASSIGN a Value (of a certain TYPE)
+	x := 42
+	fmt.Println(x)
+
+	fmt.Println(y)
+
+	foo()
+
+	fmt.Println(z)
+}
+
+func foo() {
+	fmt.Println("again:", y)
+}
