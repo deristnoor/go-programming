5e6c6c7e11cb5b157a852ae9f393a7ec4d22c449
deristnoor <deris.t.noor@gmail.com>
2020-07-24 14:01:45 +0700

    excellent

---
+++ Excercises/Level_5/Level_5.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type haha int
+
+var x haha
+var y int
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 76
+	fmt.Println(x)
+
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+
+}
